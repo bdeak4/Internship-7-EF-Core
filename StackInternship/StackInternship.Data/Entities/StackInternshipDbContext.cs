ebb01c8cc886997955084ff3445e6273dc1a6063
Bartol Deak <b@bdeak.net>
2021-12-26 21:22:15 +0100

    dbcontext setup
    https://github.com/dujesaric/EFCodeSession/commit/62aaa5f0bf509fa2b762716911ec76d58ac82514

---
+++ StackInternship/StackInternship.Data/Entities/StackInternshipDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace StackInternship.Data.Entities
+{
+    public class StackInternshipDbContext : DbContext
+    {
+        public StackInternshipDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class StackInternshipContextFactory : IDesignTimeDbContextFactory<StackInternshipDbContext>
+    {
+        public StackInternshipDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("App.config")
+                .Build();
+
+            configuration
+                .Providers
+                .First()
+                .TryGet("connectionString:add:StackInternship:connectionString", out var connectionString);
+
+            var options = new DbContextOptionsBuilder<StackInternshipDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new StackInternshipDbContext(options);
+        }
+    }
+}
+
